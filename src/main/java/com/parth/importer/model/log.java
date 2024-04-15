38cea9f0113c3f852cf044315031a0e097fea870
I3-Parth <parth.shah@techprescient.com>
2024-04-15 16:46:33 +0530

    f1: log model added

---
+++ src/main/java/com/parth/importer/model/log.java
@@ -0,0 +1,19 @@
+package com.parth.importer.model;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class log {
+    private Long id;
+    private Long studentId;
+    private String name;
+    private int age;
+    private String email;
+    private String city;
+    private Long statsCode;
+    private String statusMessage;
+}
